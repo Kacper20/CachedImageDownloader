ac5f5df053c99db821f2313bb0cfe46c3b86c482
Kacper Harasim <kacper.harasim@polidea.com>
2017-03-08 19:34:50 +0100

    Initial Commit

---
+++ CachedImageDownloader_iOS/CachedImageDownloader_iOS.h
@@ -0,0 +1,19 @@
+//
+//  CachedImageDownloader_iOS.h
+//  CachedImageDownloader_iOS
+//
+//  Created by Kacper Harasim on 08.03.2017.
+//  Copyright © 2017 Kacper Harasim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CachedImageDownloader_iOS.
+FOUNDATION_EXPORT double CachedImageDownloader_iOSVersionNumber;
+
+//! Project version string for CachedImageDownloader_iOS.
+FOUNDATION_EXPORT const unsigned char CachedImageDownloader_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CachedImageDownloader_iOS/PublicHeader.h>
+
+
